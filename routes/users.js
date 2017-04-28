265929e9b14b22aca44952c586b5c04bfd8f7aaf
Aria Moraine <ariamoraine@gmail.com>
2017-04-28 12:47:23 -0400

    adding in single user page

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const userRouter = express();
+const models = require('../models');
+const User = models.User;
+const Page = models.Page;
 
+userRouter.get('/', function (req, res, next) {
+  User.findAll()
+  .then(users => {
+    res.render('users', {
+      users: users
+    })
+  })
+  .catch(next);
+})
+
+userRouter.get('/:id', function (req, res, next) {
+  Promise.all([
+    User.findOne({where: {id: req.params.id}}),
+    Page.findAll({where: {authorId: req.params.id}})
+  ])
+  .then(results => {
+    res.render('singleUser', {user: results[0], pages: results[1]});
+  })
+  .catch(next);
+})
 
 module.exports = userRouter;
